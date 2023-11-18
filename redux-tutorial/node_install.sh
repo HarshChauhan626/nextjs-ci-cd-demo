016dfa5330fee1159c27ec6ed8510ad950a3f37f
Harsh Chauhan <60404356+HarshChauhan626@users.noreply.github.com>
2023-11-18 23:06:26 +0530

    Add files via upload

---
+++ redux-tutorial/node_install.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Install NVM (Node Version Manager)
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Install Node.js 18.18.2
+nvm install 18.18.2
+
+# Use Node.js 18.18.2 as the default version
+nvm alias default 18.18.2
+
+# Install the latest version of npm
+npm install -g npm@latest
+
+# Display Node.js and npm versions
+node -v
+npm -v
