4ab17f6fe7bee923e417f166f861ad393ed38c4b
Harsh Chauhan <60404356+HarshChauhan626@users.noreply.github.com>
2023-11-19 00:24:28 +0530

    Update project_deploy.sh

--- redux-tutorial/project_deploy.sh
+++ redux-tutorial/project_deploy.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Load NVM
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+
+# Use Node.js 18.18.2 as the default version
+nvm use 18.18.2
+
+# Source NVM initialization script to set up environment variables
+source ~/.nvm/nvm.sh
+
+# Install pm2 globally
+npm install -g pm2
+
+# Navigate to the project directory
+cd ~/my-app/redux-tutorial
+
 # Step 1: Install npm dependencies
 npm install
 
